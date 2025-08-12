eff055f0582c67c9237a8a75015254e08e93a4fd
Meowcelinee <meowcelinee@gmail.com>
2025-08-12 02:59:41 -0400

    no fuck this

--- functions/env.js
+++
@@ -1,8 +0,0 @@
-export async function url(context) {
-    console.log(`[/functions/env.js:async url()] context:`);
-    console.log(context);
-
-    const URL = context.env.REQUEST_URL;
-
-    return new Response(JSON.stringify(URL));
-}

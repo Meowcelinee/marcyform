1763b921f0de2eb858e87a7c30cfead7780a38f1
Meowcelinee <meowcelinee@gmail.com>
2025-08-12 02:16:15 -0400

    logging + .then

--- functions/env.js
+++ functions/env.js
@@ -1,4 +1,7 @@
 export async function url(context) {
+    console.log(`[/functions/env.js:async url()] context:`);
+    console.log(context);
+
     const URL = context.env.REQUEST_URL;
 
     return new Response(URL);
